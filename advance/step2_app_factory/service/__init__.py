e2860c7666809b9c9e85c8ce6a66bd47ed6127cc
chubinibini <dmdsi1@naver.com>
2023-03-27 16:50:41 +0900

    feat: wtf를 이용한 입력폼 유효성 적용 및 csrf 토큰 적용 part1

--- advance/step2_app_factory/service/__init__.py
+++ advance/step2_app_factory/service/__init__.py
@@ -8,10 +8,29 @@ from flask import Flask
 '''
 def create_app():
     app = Flask(__name__)
-
+    
+    # 환경 변수 초기화
+    init_environment(app)
+    # 블루프린트 초기화
     init_blueprint(app)
     
+    
     return app
+
+def init_environment(app):
+    # 특정파일(cfg, ...)등을 읽어서 처리 가능
+    app.config.from_pyfile('resource/config.cfg', silent=True)
+    # py을 모듈가져오기 해서 (객체)를 세팅해서 처리
+    import service.config as config
+    app.config.from_object( config )
+    # 환경변수(OS레벨, 플라스크레벨, 사용자정의레벨) 모두 출력
+    print('\n' + '-'*20)
+    # 개별 환경 변수값 추출
+    print(app.config['SECRET_KEY'], app.config.get('SECRET_KEY'))
+    # for k, v in app.config.items():
+    #     print(k,v)
+    print('-'*20 + '\n')
+    pass
 
 def init_blueprint(app):
     # app에 블루프린트 객체를 등록한다
@@ -31,21 +50,3 @@ def init_blueprint(app):
     app.register_blueprint(bp_auth)
 
     pass
-
-# def auth_blueprint(app):
-#     # app에 블루프린트 객체를 등록한다
-
-#     # 실습 http://127.0.0.1:5000/auth/ 접속시 인증홈이란 내용이 나오도록
-#     # auth 관련 블루프린트를 구성하시오
-
-#     # 블루프린트로 정의된 개별 페이지 관련 내용 로드
-#     from .controllers import main_controller
-#     # 이 위치에서는 service를 생략하고 표현가능
-#     #from service.controllers import bp_main
-#     from .controllers import bp_main, bp_auth
-
-#     # 플라스크 객체에 블루 프린트 등록
-#     app.register_blueprint(bp_main)
-#     app.register_blueprint(bp_auth)
-
-#     pass
